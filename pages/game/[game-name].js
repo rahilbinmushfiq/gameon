22eacfd7eead8a51503c292938259ada6ac10b35
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-02-15 17:31:42 +0600

    Renamed the dynamic page under the Game page; hence removed

--- pages/game/[game-name].js
+++
@@ -1,9 +0,0 @@
-export default function GameDetails() {
-  return (
-    <main>
-      <section>
-        <h1 className="text-3xl underline">Game Details</h1>
-      </section>
-    </main>
-  );
-}
