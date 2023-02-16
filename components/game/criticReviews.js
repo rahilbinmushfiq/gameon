1a230b56a0dd0437cea63a741c890a107493c45d
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-02-16 23:40:53 +0600

    Modified the previous skeleton page, and displayed the fetched gameDetails (props)

--- components/game/criticReviews.js
+++ components/game/criticReviews.js
@@ -1,5 +1,42 @@
-export default function CriticReviews() {
+export default function CriticReviews({ criticReviews: { scoresList } }) {
+    const getDate = (date) => {
+        const formattedDate = new Date(date.seconds * 1000 + date.nanoseconds / 1000000).toLocaleDateString('en-US', {
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric'
+        });
+
+        return formattedDate;
+    }
+
     return (
-        <h2 className="underline">Critic Reviews Tab</h2>
+        <>
+            <h2 className="text-2xl underline">Critic Reviews Tab</h2>
+            {scoresList.map((review) => {
+                return (
+                    <div key={review.userUID}>
+                        <div className="flex gap-[30rem]">
+                            <div className="flex gap-4">
+                                <div>
+                                    <img className="w-[3rem]" src="https://lh3.googleusercontent.com/a/AEdFTp7lFa-o7rKx5aNNxqZY8p1BOfayskkPK12m29TLdw=s288-p-rw-no" alt="user" />
+                                </div>
+                                <div>
+                                    <p>by {review.organizationName}</p>
+                                    <p>on {getDate(review.postedOn)}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <p>Score: {review.score}</p>
+                            </div>
+                        </div>
+                        <div className="w-[50rem]">
+                            {review.comment}
+                        </div>
+                        <a href={review.articleLink} target="_blank" rel="noopener noreferrer">Read full review</a>
+                    </div>
+                )
+            })}
+        </>
     );
 }
--- components/game/overview.js
+++ components/game/overview.js
@@ -1,5 +1,33 @@
-export default function Overview() {
+export default function Overview({ overview: { summary, genre, platform, releaseDate, averageRating, averageScore } }) {
+    releaseDate = new Date(releaseDate.seconds * 1000 + releaseDate.nanoseconds / 1000000).toLocaleDateString('en-US', {
+        // weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
     return (
-        <h2 className="underline">Overview Tab</h2>
+        <>
+            <h2 className="text-2xl underline">Overview Tab</h2>
+            <div className="flex gap-16">
+                <div className="w-[35rem]">
+                    <h3 className="underline">Summary</h3>
+                    <p>{summary}</p>
+                </div>
+                <div>
+                    <h3 className="underline">About</h3>
+                    <div>
+                        <p>Genres: {genre.map(g => g)}</p>
+                        <p>Platforms: {platform.map(p => p)}</p>
+                        <p>Release date: {releaseDate}</p>
+                    </div>
+                </div>
+                <div>
+                    <h3 className="underline">Review</h3>
+                    <p>Average rating: {averageRating.toFixed(2)}</p>
+                    <p>Average score: {averageScore.toFixed(1)}</p>
+                </div>
+            </div>
+        </>
     );
 }
