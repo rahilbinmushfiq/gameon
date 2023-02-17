38291f7ba9a3a37f67cd2af90093fa591d1883e9
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-02-17 16:08:35 +0600

    feat(criticReviews): Add review submission feature for logged in users
    This commit adds the ability for logged in users to submit their own reviews on the gameDetails page using the criticReviews component.

--- components/game/criticReviews.js
+++ components/game/criticReviews.js
@@ -1,4 +1,18 @@
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, updateDoc, arrayUnion, Timestamp } from "firebase/firestore";
+
 export default function CriticReviews({ criticReviews: { scoresList } }) {
+    const [user] = useAuthState(auth);
+    const router = useRouter();
+    const organizationNameRef = useRef();
+    const organizationEmailRef = useRef();
+    const scoreRef = useRef();
+    const articleLinkRef = useRef();
+    const commentRef = useRef();
+
     const getDate = (date) => {
         const formattedDate = new Date(date.seconds * 1000 + date.nanoseconds / 1000000).toLocaleDateString('en-US', {
             month: 'long',
@@ -10,9 +24,47 @@ export default function CriticReviews({ criticReviews: { scoresList } }) {
         return formattedDate;
     }
 
+    const handleCriticReview = async () => {
+        if (!user) return router.push("/signin");
+
+        let organizationName = organizationNameRef?.current?.value;
+        let organizationEmail = organizationEmailRef?.current?.value;
+        let score = scoreRef?.current?.value;
+        let articleLink = articleLinkRef?.current?.value;
+        let comment = commentRef?.current?.value;
+
+        try {
+            await updateDoc(doc(db, "games", "fifa-21"), {
+                "reviews.scores.scoresList": arrayUnion({
+                    organizationName,
+                    organizationEmail,
+                    score,
+                    articleLink,
+                    comment,
+                    postedOn: Timestamp.now(),
+                    userUID: user.uid
+                })
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
+        router.push("/game/fifa-21");
+    }
+
     return (
         <>
             <h2 className="text-2xl underline">Critic Reviews Tab</h2>
+            <div className="flex flex-col mb-[2rem]">
+                <h4 className="underline">Review this game</h4>
+                <input ref={organizationNameRef} type="text" placeholder="Name of the organization" required />
+                <input ref={organizationEmailRef} type="email" placeholder="Email of the organization" required />
+                <input ref={scoreRef} type="number" placeholder="Score (0-50)" min="0" max="50" required />
+                <input ref={articleLinkRef} type="url" placeholder="Article link" required />
+                <textarea ref={commentRef} className="w-[50rem] mb-2" placeholder="Comment" required></textarea>
+                <button onClick={() => handleCriticReview()} className="w-fit text-white bg-slate-600">Review</button>
+            </div>
+            <h4 className="text-xl underline">Critic Reviews</h4>
             {scoresList.map((review) => {
                 return (
                     <div key={review.userUID}>
