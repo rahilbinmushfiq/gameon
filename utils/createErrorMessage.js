1340f5989ed40db42f7edde5e8a8b58d52388103
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-02-20 19:33:16 +0600

    feat(utils): Add error message utility function
    Create a new function createErrorMessage in the utils folder which takes an error object and returns a string with the error message. This function will be used later in pages, or components to display error messages for better user experience.

---
+++ utils/createErrorMessage.js
@@ -0,0 +1,29 @@
+export default function createErrorMessage(error) {
+    switch (error.code) {
+        case 'auth/weak-password':
+            return "The password must be at least 6 characters long.";
+        case 'auth/wrong-password':
+            return "The password is incorrect.";
+        case 'auth/popup-blocked':
+            return "Sign in unsuccessful: The popup window was blocked by the browser.";
+        case 'auth/popup-closed-by-user':
+        case 'auth/cancelled-popup-request':
+            return "Sign in unsuccessful: The popup window was closed before completing the sign-in process.";
+        case 'auth/network-request-failed':
+            return "A network error occurred. Please try again.";
+        case 'auth/user-disabled':
+            return "This user account has been disabled.";
+        case 'auth/internal-error':
+            return "An internal error occured in the server. Please try again";
+        case 'auth/account-exists-with-different-credential':
+            return "Sign in unsuccessful: There is already an account associated with the same email address.";
+        case 'auth/user-not-found':
+            return "There is no user corresponding to the email address provided.";
+        case 'auth/email-already-in-use':
+            return "The email address is already in use by another account.";
+        case 'auth/invalid-email':
+            return "The email address is not valid.";
+        default:
+            return "An error has occurred. Please try again.";
+    }
+}
