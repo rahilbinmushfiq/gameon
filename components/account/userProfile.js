e16117950de3555dc166acc860eca4047f22cdc0
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-02-25 18:55:05 +0600

    feat(account): add server-side authentication and user profile component
    Implemented server-side authentication with Firebase Admin SDK in the account.js page's getServerSideProps() to verify the user with the token provided from cookies. Added a user profile component (userProfile.js) to display the user's information and update the user's name if needed.

---
+++ components/account/userProfile.js
@@ -0,0 +1,87 @@
+import { updateProfile } from "firebase/auth";
+import { useRef, useState } from "react";
+import { useAuth } from "../../config/auth";
+import createErrorMessage from "../../utils/createErrorMessage";
+
+export default function UserProfile() {
+  const { user } = useAuth();
+  const [isReadOnly, setIsReadOnly] = useState(true);
+  const fullNameRef = useRef("");
+
+  const updateUserProfile = async () => {
+    try {
+      await updateProfile(user, {
+        displayName: fullNameRef?.current?.value
+      });
+    } catch (error) {
+      console.log(createErrorMessage(error));
+    }
+
+    setIsReadOnly(true);
+  }
+
+  if (user) return (
+    <section>
+      <div className="flex flex-col gap-10 mt-6 justify-center items-center">
+        <div className="flex flex-col gap-2 justify-center items-center">
+          <h1 className="text-2xl">Welcome, {user.displayName}</h1>
+          <p className="w-[28rem]">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum nihil veritatis ipsum necessitatibus recusandae animi?
+          </p>
+        </div>
+        <img className="w-[8rem] rounded-full" src={user.photoURL} alt="user" referrerPolicy="no-referrer" />
+        <div>
+          <div className="flex gap-2">
+            <label className="flex gap-2">
+              <p className="text-xl h-8">Name:</p>
+              <input
+                className={`w-[15rem] h-8 bg-white ${isReadOnly ? "border-b" : "border"} border-neutral-900`}
+                type="text"
+                disabled={isReadOnly}
+                ref={fullNameRef}
+                defaultValue={user.displayName}
+              />
+            </label>
+            <div className="flex gap-1 w-[10rem]">
+              {isReadOnly ? (
+                <button
+                  className="text-white bg-neutral-900 w-[5rem] h-8"
+                  onClick={() => setIsReadOnly(false)}
+                >
+                  Edit
+                </button>
+              ) : (
+                <>
+                  <button
+                    className="text-white bg-neutral-900 w-[5rem] h-8"
+                    onClick={updateUserProfile}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="text-white bg-neutral-900 w-[5rem] h-8"
+                    onClick={() => {
+                      setIsReadOnly(true);
+                      fullNameRef.current.value = user.displayName;
+                    }}
+                  >
+                    Cancel
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+          <label className="flex gap-2">
+            <p className="text-xl h-8">Eamil:</p>
+            <input
+              className="w-[15.5rem] h-8 bg-white border-b border-neutral-900"
+              type="email"
+              disabled={true}
+              value={user.email}
+            />
+          </label>
+        </div>
+      </div>
+    </section>
+  )
+}
--- pages/account.js
+++ pages/account.js
@@ -1,9 +1,72 @@
-export default function Account() {
-    return (
-        <main>
-            <section>
-                <h1 className="text-3xl underline">Account Info</h1>
-            </section>
-        </main>
-    )
+import nookies from "nookies";
+import { onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase-admin/auth";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { auth } from "../config/firebase";
+import { adminApp } from "../config/firebaseAdmin";
+import { useAuth } from "../config/auth";
+import UserProfile from "../components/account/userProfile";
+
+export default function Account({ userData }) {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (!user) {
+        router.push("/signin");
+      }
+    })
+  }, [user]);
+
+  return (
+    <main>
+      <UserProfile userData={userData} />
+    </main>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context);
+
+  if (!cookies.token) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    const adminAuth = getAuth(adminApp);
+    const token = await adminAuth.verifyIdToken(cookies.token);
+
+    const { name, picture, email, email_verified, uid } = token;
+
+    if (!email_verified) {
+      return {
+        redirect: {
+          destination: "/signin",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        userData: {
+          name, picture, email
+        }
+      }
+    }
+  } catch (error) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
 }
