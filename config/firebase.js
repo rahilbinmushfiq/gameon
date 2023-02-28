bc75ff83a082ff2fa9313bd19ad5068116811766
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-02-28 22:44:03 +0600

    feat(config): add firebase storage

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -24,3 +25,4 @@ const app = getFirebaseApp();
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export const storage = getStorage(app);
