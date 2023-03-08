2179125502e0d586da936844d0f6197caa23aecb
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-03-08 18:04:18 +0600

    feat: add loading for authentication and page navigation
    This commit adds a loading spinner to the Layout component when useAuth hook is still loading user authentication data, and also adds a loading spinner to the _app.js component during page navigation. The spinner component used is HashLoader from the react-spinners library.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,39 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react'
 import Layout from '../components/layout'
 import { AuthProvider } from '../config/auth'
 import '../styles/globals.css'
+import { HashLoader } from "react-spinners";
 
 function MyApp({ Component, pageProps }) {
+  const [isPageLoading, setIsPageLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => setIsPageLoading(true));
+    router.events.on("routeChangeComplete", () => setIsPageLoading(false));
+
+    return () => {
+      router.events.off("routeChangeStart", () => setIsPageLoading(true));
+      router.events.off("routeChangeComplete", () => setIsPageLoading(false));
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <Layout>
-        <Component {...pageProps} />
+        {isPageLoading ? (
+          <div className="min-h-screen max-w-screen flex justify-center items-center">
+            <HashLoader
+              color="rgb(225 29 72)"
+              loading={isPageLoading}
+              size={50}
+              speedMultiplier={2}
+            />
+          </div>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </Layout>
     </AuthProvider>
   )
