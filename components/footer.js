dd9d713ea3a642e6b1d8c88e14de2d6de8fd46de
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-03-10 12:53:55 +0600

    feat(layout): update navbar and footer components
    This commit mainly refactors navbar and footer components to improve the overall design and user experience. Their layouts are restructed and styled with some addition of new features.
    
    Modify the footer component to:
    - use Link instead of Image for logo
    - add social media links with icons
    - add copyright section
    - add styles based on mobile-first approach
    
    Update the navbar component to:
    - add a hamburger menu
    - show navbar modal when the menu icon is clicked, otherwise, hide it
    - use Link instead of Image for logo
    - add react-icons to page navigation links
    - add styles based on mobile-first approach
    
    Additionally, update the global styles in styles/globals.css to include new styles for the footer and navbar components.

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import Logo from "../public/logo.png";
+import { FaDiscord, FaFacebookSquare, FaTwitterSquare } from "react-icons/fa";
 
 export default function Footer() {
   const router = useRouter();
@@ -15,31 +15,58 @@ export default function Footer() {
   }
 
   return (
-    <footer className="flex gap-16">
-      <div className="w-36 h-12 relative">
-        <Image className="object-cover" src={Logo} fill sizes="200px" alt="gamon-logo" />
+    <footer className="max-w-screen">
+      <div className="p-6 space-y-12">
+        <div className="space-y-2">
+          <p className="text-3xl font-extrabold">
+            <Link href="/">
+              GAME <span className="text-[#e30e30]">ON</span>
+            </Link>
+          </p>
+          <p className="text-[#9f9f9f]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla non massa id consequat. Mauris sed nulla quis urna hendrerit accumsan vitae sit amet nisi.
+          </p>
+          <div className="flex gap-4 pt-4">
+            <a className="footer--a" href="https://www.discord.com" target="_blank" rel="noopener noreferrer">
+              <FaDiscord size={20} color="#1f1f1f" />
+            </a>
+            <a className="footer--a" href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+              <FaFacebookSquare size={20} color="#1f1f1f" />
+            </a>
+            <a className="footer--a" href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+              <FaTwitterSquare size={20} color="#1f1f1f" />
+            </a>
+          </div>
+        </div>
+        <div className="space-y-12 [&>*]:space-y-1">
+          <div>
+            <h2 className="font-bold tracking-wider">SEARCH GAMES</h2>
+            <ul className="space-y-1">
+              <li className="footer--li" onClick={() => handleSearchGames("pc")}>
+                Available on PC
+              </li>
+              <li className="footer--li" onClick={() => handleSearchGames("playstation")}>
+                Available on Playstaion
+              </li>
+              <li className="footer--li" onClick={() => handleSearchGames("xbox")}>
+                Available on Xbox
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-bold tracking-wider">OUR OFFICE</h2>
+            <p className="text-[#9f9f9f]">
+              458 West Green Hill St., NC 27540
+            </p>
+          </div>
+          <div>
+            <h2 className="font-bold tracking-wider">CONTACT US</h2>
+            <p className="text-[#9f9f9f]">game.on.official@gmail.com</p>
+          </div>
+        </div>
       </div>
-      <div className="w-[15rem]">
-        <h2 className="font-semibold">SEARCH GAMES</h2>
-        <p className="cursor-pointer hover:bg-gray-100 w-fit" onClick={() => handleSearchGames("pc")}>
-          Available on PC
-        </p>
-        <p className="cursor-pointer hover:bg-gray-100 w-fit" onClick={() => handleSearchGames("playstation")}>
-          Available on Playstaion
-        </p>
-        <p className="cursor-pointer hover:bg-gray-100 w-fit" onClick={() => handleSearchGames("xbox")}>
-          Available on Xbox
-        </p>
-      </div>
-      <div className="w-[15rem]">
-        <h2 className="font-semibold">OUR OFFICE</h2>
-        <p>
-          458 West Green Hill St. Holly Springs, NC 27540
-        </p>
-      </div>
-      <div className="w-[15rem]">
-        <h2 className="font-semibold">CONTACT US</h2>
-        <p>game.on.official@gmail.com</p>
+      <div className="max-w-screen px-6 py-3 bg-[#2f2f2f]">
+        <p className="text-[#f1f1f1] text-center text-sm">Copyright &copy; <span className="text-[#e30e30]">Game On</span> - 2023. All Rights Reserved</p>
       </div>
     </footer>
   )
--- components/navbar.js
+++ components/navbar.js
@@ -1,28 +1,103 @@
-import Logo from "../public/logo.png";
 import { auth } from "../config/firebase";
-import Image from "next/image";
 import Link from "next/link";
 import { useAuth } from "../config/auth";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { IoMenuOutline, IoHomeOutline, IoGameControllerOutline, IoPersonOutline, IoLogInOutline, IoLogOutOutline } from "react-icons/io5"
 
 export default function Navbar() {
-    const { user, isLoading } = useAuth();
+  const { user } = useAuth();
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const { asPath } = useRouter();
 
-    if (isLoading) return <h1>Loading...</h1>;
-
-    return (
-        <header className="mb-2">
-            <div className=" w-28 h-10 relative">
-                <Image className="object-cover" src={Logo} fill sizes="200px" alt="gamon-logo" />
+  return (
+    <header className="px-6 py-4">
+      <nav>
+        <button
+          onClick={() => setIsNavbarOpen(prevState => !prevState)}
+        >
+          <IoMenuOutline size={30} color="#f1f1f1" />
+        </button>
+        {isNavbarOpen && (
+          <div
+            className="fixed inset-0 w-screen h-screen bg-[#3f3f3f] bg-opacity-25 backdrop-blur"
+            id="navbar-bg"
+            onClick={(event) => event.target.id === "navbar-bg" && setIsNavbarOpen(prevState => !prevState)}
+          >
+            <div className="w-3/5 h-screen p-6 bg-[#1f1f1f] rounded-r-[1rem]">
+              <p className="text-3xl font-extrabold mb-10">
+                <Link href="/">
+                  GAME <span className="text-[#e30e30]">ON</span>
+                </Link>
+              </p>
+              <ul className="flex flex-col gap-2">
+                <Link href="/">
+                  <li className="nav--li">
+                    <IoHomeOutline
+                      size={20}
+                      color={`${asPath === "/" ? "#f1f1f1" : "#9f9f9f"}`}
+                    />
+                    <p className={`${asPath === "/" ? "font-bold text-[#f1f1f1]" : "font-semibold text-[#9f9f9f]"}`}>
+                      Home
+                    </p>
+                  </li>
+                </Link>
+                <Link href="/search-games">
+                  <li className="nav--li">
+                    <IoGameControllerOutline
+                      size={20}
+                      color={`${asPath === "/search-games" ? "#f1f1f1" : "#9f9f9f"}`}
+                    />
+                    <p className={`${asPath === "/search-games" ? "font-bold text-[#f1f1f1]" : "font-semibold text-[#9f9f9f]"}`}>
+                      Games
+                    </p>
+                  </li>
+                </Link>
+                {(!user || (user && !user.emailVerified)) && (
+                  <Link href="/signin">
+                    <li className="nav--li">
+                      <IoLogInOutline
+                        size={20}
+                        color={`${asPath === "/signin" ? "#f1f1f1" : "#9f9f9f"}`}
+                      />
+                      <p className={`${asPath === "/signin" ? "font-bold text-[#f1f1f1]" : "font-semibold text-[#9f9f9f]"}`}>
+                        Sign in
+                      </p>
+                    </li>
+                  </Link>
+                )}
+                {user && user.emailVerified && (
+                  <>
+                    <Link href="/account">
+                      <li className="nav--li">
+                        <IoPersonOutline
+                          size={20}
+                          color={`${asPath === "/account" ? "#f1f1f1" : "#9f9f9f"}`}
+                        />
+                        <p className={`${asPath === "/account" ? "font-bold text-[#f1f1f1]" : "font-semibold text-[#9f9f9f]"}`}>
+                          Account
+                        </p>
+                      </li>
+                    </Link>
+                    <li
+                      className="nav--li cursor-pointer"
+                      onClick={() => auth.signOut()}
+                    >
+                      <IoLogOutOutline
+                        size={20}
+                        color="#9f9f9f"
+                      />
+                      <p className="font-semibold text-[#9f9f9f]">
+                        Sign out
+                      </p>
+                    </li>
+                  </>
+                )}
+              </ul>
             </div>
-            <nav>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/search-games">Games</Link></li>
-                    <li><Link href="/account">Account</Link></li>
-                    {(!user || (user && !user.emailVerified)) && <li><Link href="/signin">Sign in</Link></li>}
-                    {user && user.emailVerified && <li onClick={() => auth.signOut()}>Sign out</li>}
-                </ul>
-            </nav>
-        </header>
-    )
+          </div>
+        )}
+      </nav>
+    </header>
+  )
 }
