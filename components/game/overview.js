9ab6d7622a4199433ce3205251cb200e6aa50d2f
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-03-15 23:46:58 +0600

    style(overview): add styles to Overview component
    Add styles to the Overview component for better readability and user experience. The changes include:
    - Change the layout of the component to suit the mobile first approach design
    - Add background colors to different sections
    - Adjust font size, weight and color to achieve visual hierarchy
    - Display the average user rating as stars
    - Display the average critic score in a box (div)

--- components/game/overview.js
+++ components/game/overview.js
@@ -2,27 +2,43 @@ import { getDate } from "../../utils/convertTimestamp";
 
 export default function Overview({ overview: { summary, genres, platforms, releaseDate, averageRating, averageScore } }) {
   return (
-    <>
-      <h2 className="text-2xl underline">Overview Tab</h2>
-      <div className="flex gap-16">
-        <div className="w-[35rem]">
-          <h3 className="underline">Summary</h3>
-          <p>{summary}</p>
-        </div>
-        <div>
-          <h3 className="underline">About</h3>
-          <div>
-            <p>Genres: {genres.join(", ")}</p>
-            <p>Platforms: {platforms.join(", ")}</p>
-            <p>Release date: {getDate(releaseDate)}</p>
-          </div>
-        </div>
-        <div>
-          <h3 className="underline">Review</h3>
-          <p>Average rating: {averageRating.toFixed(2)}</p>
-          <p>Average score: {averageScore.toFixed(1)}</p>
+    <section className="px-6 space-y-4">
+      <div className="p-4 bg-[#2a2a2a] space-y-2">
+        <h3 className="text-lg font-bold">Summary</h3>
+        <p className="text-[#a9a9a9]">{summary}</p>
+      </div>
+      <div className="p-4 bg-[#2a2a2a] space-y-2">
+        <h3 className="text-lg font-bold">About</h3>
+        <div className="space-y-3">
+          <p className="text-[#a9a9a9]"><span className="font-bold">Genres:</span> {genres.join(", ")}</p>
+          <p className="text-[#a9a9a9]"><span className="font-bold">Platforms:</span> {platforms.join(", ")}</p>
+          <p className="text-[#a9a9a9]"><span className="font-bold">Release date:</span> {getDate(releaseDate)}</p>
         </div>
       </div>
-    </>
+      <div className="p-4 bg-[#2a2a2a] space-y-1">
+        <h3 className="text-lg font-bold">Reviews</h3>
+        <div className="space-y-6">
+          <div className="space-y-1">
+            <p className="font-bold text-[#a9a9a9]">Average user rating <span className="font-semibold">(out of 5)</span>:</p>
+            <div className="flex items-center gap-2">
+              <div className="relative text-[#a9a9a9] inline-block overflow-hidden">
+                <div className="before:content-['\2605\2605\2605\2605\2605'] before:text-[18pt]" />
+                <div
+                  className="before:content-['\2605\2605\2605\2605\2605'] before:text-[18pt] absolute inset-0 text-[#e30e30] overflow-hidden"
+                  style={{ width: `${averageRating * 20}%` }}
+                />
+              </div>
+              <p className="text-[#a9a9a9] font-semibold">({averageRating})</p>
+            </div>
+          </div>
+          <div className="space-y-2">
+            <p className="font-bold text-[#a9a9a9]">Average critic score <span className="font-semibold">(out of 50)</span>:</p>
+            <div className="p-3 bg-[#e30e30] text-[#f1f1f1] font-semibold inline-block">
+              {averageScore.toFixed(1)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
