33b5b65f4146438feaeac3af1e16896779e2645d
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-03-15 12:50:37 +0600

    feat(gameCard): improve game card UI for better UX
    - Update average rating display to show stars and rating as percentage
    - Update average score display to show score in a box with a red background
    - Add new space-y-2 div to separate rating and score display
    
    Additional refactors:
    - Change the size and color of the game title
    - Remove "mr-4" from thumbnail container to fix unwanted-gap bug

--- components/searchGames/gameCard.js
+++ components/searchGames/gameCard.js
@@ -7,21 +7,35 @@ export default function GameCard({ game: { id, thumbnailURL, name, summary, plat
   return (
     <Link href={`/game/${id}`}>
       <div className="mb-6 grid grid-rows-2 gap-4 m-6 p-4 bg-[#353535]">
-        <div className="relative rounded-sm overflow-hidden mr-4">
+        <div className="relative rounded-sm overflow-hidden">
           <Image className="object-cover" src={thumbnailURL} fill sizes="10rem" alt={`${name} thumbnail`} />
         </div>
         <div className="space-y-4 text-[#cfcfcf]">
           <div>
-            <h3 className="text-lg font-bold">{name} <span className="font-semibold">({getYear(releaseDate)})</span></h3>
+            <h3 className="text-xl font-bold text-[#f1f1f1]">{name} <span className="font-semibold">({getYear(releaseDate)})</span></h3>
             <p className="font-semibold">{platforms.join(" • ")}</p>
           </div>
           <h4 className="text-xs">{sliceParagraph(summary)}</h4>
           <div className="py-1">
             <hr className="border-[#4a4a4a]" />
           </div>
-          <div>
-            <p className="font-semibold">Average rating: {averageRating}/5</p>
-            <p className="font-semibold">Average score: {averageScore}/50</p>
+          <div className="space-y-2">
+            <div className="space-y-1">
+              <p className="font-semibold">Average user rating:</p>
+              <div className="relative text-[#a9a9a9] inline-block overflow-hidden">
+                <div className="before:content-['\2605\2605\2605\2605\2605'] before:text-[20pt]" />
+                <div
+                  className="before:content-['\2605\2605\2605\2605\2605'] before:text-[20pt] absolute inset-0 text-[#e30e30] overflow-hidden"
+                  style={{ width: `${averageRating * 20}%` }}
+                />
+              </div>
+            </div>
+            <div className="space-y-2">
+              <p className="font-semibold">Average critic score:</p>
+              <div className="px-3 py-3 bg-[#e30e30] text-[#f1f1f1] font-bold inline-block">
+                {averageScore.toFixed(1)}
+              </div>
+            </div>
           </div>
         </div>
       </div>
