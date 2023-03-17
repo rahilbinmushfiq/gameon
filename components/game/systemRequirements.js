4f2a66c90fd8b340fa2955f1506697f25a8ca07e
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-03-17 13:02:21 +0600

    feat(systemRequirements): add styled table
    This commit introduces a new styled system requirements table for the game page's SystemRequirements component. The purpose behind updating the component is to make it more readable and user-friendly. The layout is refactored using a table and styled to improve the presentation.
    
    Changes Made:
    - Replace the old system requirements design with a new styled table for better organization
    - Remove the divs used to display the information and replace them with table cells
    - Add sticky header to table for easy navigation and responsiveness
    - Style the header with a red underline
    - Add background colors

--- components/game/systemRequirements.js
+++ components/game/systemRequirements.js
@@ -1,43 +1,61 @@
 export default function SystemRequirements({ systemRequirements: { minimum, recommended } }) {
   return (
-    <>
-      <h2 className="text-2xl underline">System Requirements Tab</h2>
-      <div className="flex gap-16">
-        <div>
-          <h3 className="underline">Component</h3>
-          <p>Processor:</p><br />
-          <p>Graphics Card:</p><br />
-          <p>RAM:</p>
-          <p>VRAM:</p>
-          <p>OS:</p>
-          <p>DirectX:</p>
-          <p>HDD Space:</p>
-        </div>
-        <div>
-          <h3 className="underline">Minimum</h3>
-          <p>{minimum.processors[0]}</p>
-          <p>{minimum.processors[1]}</p>
-          <p>{minimum.graphicsCards[0]}</p>
-          <p>{minimum.graphicsCards[1]}</p>
-          <p>{minimum.ram}</p>
-          <p>{minimum.vram}</p>
-          <p>{minimum.os}</p>
-          <p>{minimum.dx}</p>
-          <p>{minimum.hdd}</p>
-        </div>
-        <div>
-          <h3 className="underline">Recomended</h3>
-          <p>{recommended.processors[0]}</p>
-          <p>{recommended.processors[1]}</p>
-          <p>{recommended.graphicsCards[0]}</p>
-          <p>{recommended.graphicsCards[1]}</p>
-          <p>{recommended.ram}</p>
-          <p>{recommended.vram}</p>
-          <p>{recommended.os}</p>
-          <p>{recommended.dx}</p>
-          <p>{recommended.hdd}</p>
-        </div>
-      </div>
-    </>
+    <section className="px-6 py-8 bg-[#2a2a2a]">
+      <h1 className="inline-block mb-8 text-lg font-bold relative after:content-[''] after:absolute after:h-[3px] after:w-1/4 after:-bottom-1 after:left-0 after:bg-[#e30e30]">
+        System Requirements
+      </h1>
+      <div className="bg-[#3a3a3a] rounded-md overflow-auto">
+        <table>
+          <tr>
+            <th className="sticky inset-0 bg-[#3a3a3a]">Component</th>
+            <th>Minimum</th>
+            <th>Recommended</th>
+          </tr>
+          <tr>
+            <th className="sticky inset-0 bg-[#3a3a3a]" rowspan="2">Processor:</th>
+            <td>{minimum.processors[0]}</td>
+            <td>{recommended.processors[0]}</td>
+          </tr>
+          <tr>
+            <td>{minimum.processors[1]}</td>
+            <td>{recommended.processors[1]}</td>
+          </tr>
+          <tr>
+            <th className="sticky inset-0 bg-[#3a3a3a]" rowspan="2">Graphics Card:</th>
+            <td>{minimum.graphicsCards[0]}</td>
+            <td>{recommended.graphicsCards[0]}</td>
+          </tr>
+          <tr>
+            <td>{minimum.graphicsCards[1]}</td>
+            <td>{recommended.graphicsCards[1]}</td>
+          </tr>
+          <tr>
+            <th className="sticky inset-0 bg-[#3a3a3a]">RAM:</th>
+            <td>{minimum.ram}</td>
+            <td>{recommended.ram}</td>
+          </tr>
+          <tr>
+            <th className="sticky inset-0 bg-[#3a3a3a]">VRAM:</th>
+            <td>{minimum.vram}</td>
+            <td>{recommended.vram}</td>
+          </tr>
+          <tr>
+            <th className="sticky inset-0 bg-[#3a3a3a]">OS:</th>
+            <td>{minimum.os}</td>
+            <td>{recommended.os}</td>
+          </tr>
+          <tr>
+            <th className="sticky inset-0 bg-[#3a3a3a]">DirectX:</th>
+            <td>{minimum.dx}</td>
+            <td>{recommended.dx}</td>
+          </tr>
+          <tr>
+            <th className="sticky inset-0 bg-[#3a3a3a]">HDD Space:</th>
+            <td>{minimum.hdd}</td>
+            <td>{recommended.hdd}</td>
+          </tr>
+        </table>
+      </div >
+    </section>
   );
 }
