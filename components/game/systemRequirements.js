ec4c1fcbf6ca98ed82575d316671794960d6890a
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-03-19 01:47:00 +0600

    fix(systemRequirements): fix rowspan and <tbody> warnings
    Fix warnings related to DOM property and nesting:
    - Change "rowspan" to "rowSpan" to fix invalid DOM property warning
    - Add tbody element to match the DOM tree generated by the browser and fix the validateDOMNesting warning

--- components/game/systemRequirements.js
+++ components/game/systemRequirements.js
@@ -6,54 +6,56 @@ export default function SystemRequirements({ systemRequirements: { minimum, reco
       </h1>
       <div className="bg-[#3a3a3a] rounded-md overflow-auto">
         <table>
-          <tr>
-            <th className="sticky inset-0 bg-[#3a3a3a]">Component</th>
-            <th>Minimum</th>
-            <th>Recommended</th>
-          </tr>
-          <tr>
-            <th className="sticky inset-0 bg-[#3a3a3a]" rowspan="2">Processor:</th>
-            <td>{minimum.processors[0]}</td>
-            <td>{recommended.processors[0]}</td>
-          </tr>
-          <tr>
-            <td>{minimum.processors[1]}</td>
-            <td>{recommended.processors[1]}</td>
-          </tr>
-          <tr>
-            <th className="sticky inset-0 bg-[#3a3a3a]" rowspan="2">Graphics Card:</th>
-            <td>{minimum.graphicsCards[0]}</td>
-            <td>{recommended.graphicsCards[0]}</td>
-          </tr>
-          <tr>
-            <td>{minimum.graphicsCards[1]}</td>
-            <td>{recommended.graphicsCards[1]}</td>
-          </tr>
-          <tr>
-            <th className="sticky inset-0 bg-[#3a3a3a]">RAM:</th>
-            <td>{minimum.ram}</td>
-            <td>{recommended.ram}</td>
-          </tr>
-          <tr>
-            <th className="sticky inset-0 bg-[#3a3a3a]">VRAM:</th>
-            <td>{minimum.vram}</td>
-            <td>{recommended.vram}</td>
-          </tr>
-          <tr>
-            <th className="sticky inset-0 bg-[#3a3a3a]">OS:</th>
-            <td>{minimum.os}</td>
-            <td>{recommended.os}</td>
-          </tr>
-          <tr>
-            <th className="sticky inset-0 bg-[#3a3a3a]">DirectX:</th>
-            <td>{minimum.dx}</td>
-            <td>{recommended.dx}</td>
-          </tr>
-          <tr>
-            <th className="sticky inset-0 bg-[#3a3a3a]">HDD Space:</th>
-            <td>{minimum.hdd}</td>
-            <td>{recommended.hdd}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <th className="sticky inset-0 bg-[#3a3a3a]">Component</th>
+              <th>Minimum</th>
+              <th>Recommended</th>
+            </tr>
+            <tr>
+              <th className="sticky inset-0 bg-[#3a3a3a]" rowSpan="2">Processor:</th>
+              <td>{minimum.processors[0]}</td>
+              <td>{recommended.processors[0]}</td>
+            </tr>
+            <tr>
+              <td>{minimum.processors[1]}</td>
+              <td>{recommended.processors[1]}</td>
+            </tr>
+            <tr>
+              <th className="sticky inset-0 bg-[#3a3a3a]" rowSpan="2">Graphics Card:</th>
+              <td>{minimum.graphicsCards[0]}</td>
+              <td>{recommended.graphicsCards[0]}</td>
+            </tr>
+            <tr>
+              <td>{minimum.graphicsCards[1]}</td>
+              <td>{recommended.graphicsCards[1]}</td>
+            </tr>
+            <tr>
+              <th className="sticky inset-0 bg-[#3a3a3a]">RAM:</th>
+              <td>{minimum.ram}</td>
+              <td>{recommended.ram}</td>
+            </tr>
+            <tr>
+              <th className="sticky inset-0 bg-[#3a3a3a]">VRAM:</th>
+              <td>{minimum.vram}</td>
+              <td>{recommended.vram}</td>
+            </tr>
+            <tr>
+              <th className="sticky inset-0 bg-[#3a3a3a]">OS:</th>
+              <td>{minimum.os}</td>
+              <td>{recommended.os}</td>
+            </tr>
+            <tr>
+              <th className="sticky inset-0 bg-[#3a3a3a]">DirectX:</th>
+              <td>{minimum.dx}</td>
+              <td>{recommended.dx}</td>
+            </tr>
+            <tr>
+              <th className="sticky inset-0 bg-[#3a3a3a]">HDD Space:</th>
+              <td>{minimum.hdd}</td>
+              <td>{recommended.hdd}</td>
+            </tr>
+          </tbody>
         </table>
       </div >
     </section>
