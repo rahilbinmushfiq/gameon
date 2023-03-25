fb3355d4dc1287dd5d25d42388bed538e8476897
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-03-25 17:02:07 +0600

    feat(verification): add email resend countdown
    This commit adds a countdown timer to the email verification process, which disables the "Resend email" button for 60 seconds after clicking it. The remaining time is displayed alongside the button, and the button's appearance changes when it's disabled.
    
    The goal is to prevent users from spamming the "Resend email" button and to give them feedback on when they can try again.

--- components/signIn/verification.js
+++ components/signIn/verification.js
@@ -1,11 +1,25 @@
 import { sendEmailVerification } from "firebase/auth";
-import { BsFillSendFill } from "react-icons/bs"
+import { useEffect, useState } from "react";
+import { BsFillSendFill, BsStopwatchFill } from "react-icons/bs"
 import { toast } from "react-toastify";
 import { useLoading } from "../../contexts/loading";
 import createErrorMessage from "../../utils/createErrorMessage";
 
 export default function Verification({ user }) {
   const { setIsPageLoading } = useLoading();
+  const [countdown, setCountdown] = useState(60);
+
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      setCountdown(countdown => countdown - 1);
+    }, 1000);
+
+    if (countdown === 0) {
+      clearInterval(countdownInterval);
+    }
+
+    return () => clearInterval(countdownInterval);
+  }, [countdown]);
 
   const handleLinkResend = () => {
     setIsPageLoading(true);
@@ -18,7 +32,9 @@ export default function Verification({ user }) {
     }
 
     setIsPageLoading(false);
+    setCountdown(60);
   }
+
   return (
     <>
       <div className="space-y-3">
@@ -29,14 +45,25 @@ export default function Verification({ user }) {
       </div>
       <div className="space-y-4">
         <h4 className="text-[#a9a9a9]">If you don't see it, you may check your spam folder. Still can't find our email?</h4>
-        <button
-          className="flex gap-2 justify-center items-center w-40 h-12 rounded-sm text-[#1f1f1f] bg-[#f1f1f1]"
-          type="button"
-          onClick={handleLinkResend}
-        >
-          <p className="font-semibold">Resend email</p>
-          <BsFillSendFill size={15} color="#1f1f1f" />
-        </button>
+        <div className="flex gap-6 items-center">
+          <button
+            className={`flex gap-2 justify-center items-center w-40 h-12 rounded-sm text-[#1f1f1f] bg-[#f1f1f1] ${countdown > 0 ? "cursor-not-allowed hover:bg-[#a9a9a9]" : ""}`}
+            type="button"
+            disabled={countdown > 0}
+            onClick={handleLinkResend}
+          >
+            <p className="font-semibold">Resend email</p>
+            <BsFillSendFill size={15} color="#1f1f1f" />
+          </button>
+          {countdown > 0 && (
+            <div className="flex gap-2 justify-center items-center">
+              <BsStopwatchFill size={15} color="#a9a9a9" />
+              <p className="font-bold text-[#a9a9a9]">
+                after {countdown >= 60 ? "01:00" : `00:${countdown < 10 ? `0${countdown}` : countdown}`}
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
