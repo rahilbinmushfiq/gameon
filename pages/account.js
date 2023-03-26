8a8022a64a87ccdac4525446675e142ea2b5867b
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-03-26 20:34:22 +0600

    feat(account): add sign out button and functionality
    This commit adds a sign out button to the account page, which allows the user to sign out of their account. Upon clicking the button, userSignOut() will be called. With the use of this function, the user will be signed out and a success or error toast message will be displayed accordingly. The loading state will be updated during the sign-out process. Additionally, this commit adds an additional styles (min height, and divider line for each component).

--- pages/account.js
+++ pages/account.js
@@ -1,11 +1,14 @@
 import nookies from "nookies";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { getAuth } from "firebase-admin/auth";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { auth } from "../config/firebase";
 import { adminApp } from "../config/firebaseAdmin";
 import { useAuth } from "../contexts/auth";
+import { useLoading } from "../contexts/loading";
+import { toast } from "react-toastify";
+import { FaSignOutAlt } from "react-icons/fa";
 import UserProfile from "../components/account/userProfile";
 import UpdatePassword from "../components/account/updatePassword";
 import DeleteAccount from "../components/account/deleteAccount";
@@ -13,6 +16,7 @@ import Head from "next/head";
 
 export default function Account({ signInProvider }) {
   const { user } = useAuth();
+  const { setIsPageLoading } = useLoading();
   const router = useRouter();
 
   useEffect(() => {
@@ -25,14 +29,38 @@ export default function Account({ signInProvider }) {
     unsubscribe();
   }, [user]);
 
+  const userSignOut = async () => {
+    setIsPageLoading(true);
+
+    try {
+      await signOut(auth);
+      toast.success("Successfully signed out.");
+    } catch (error) {
+      toast.error("Failed to sign out. Please try again.");
+    }
+
+    setIsPageLoading(false);
+  }
+
   return (
-    <main className="max-w-full px-6 py-12">
+    <main className="max-w-full min-h-[calc(100vh_-_3.5rem)] flex flex-col divide-y-2 divide-[#2f2f2f]">
       <Head>
         <title>{`${user && `${user.displayName} |`} Game On`}</title>
       </Head>
       <UserProfile user={user} />
       <UpdatePassword user={user} signInProvider={signInProvider} />
       <DeleteAccount user={user} signInProvider={signInProvider} />
+      {user && (
+        <div className="flex-grow p-6 bg-[#2a2a2a] flex items-center">
+          <button
+            className="flex justify-center items-center gap-2 w-full h-12 rounded-sm font-semibold text-[#f1f1f1] bg-[#e30e30] hover:text-[#e30e30] hover:bg-[#f1f1f1] [&>*]:hover:fill-[#e30e30]"
+            onClick={userSignOut}
+          >
+            <p>Sign Out</p>
+            <FaSignOutAlt size={15} color="#f1f1f1" />
+          </button>
+        </div>
+      )}
     </main>
   )
 }
