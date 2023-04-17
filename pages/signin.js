e3713ee9fcc27f88dafa1c4ad811909a0b34af41
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-04-17 17:57:34 +0600

    refactor(signin): merge two <main> elements into one
    Previously, the sign in page was divided into two <main> sections, each with a <title> and a <section> inside, which were conditionally rendered based on if statements. This commit merges the two sections into a single <main>, with the contents inside <title> and <section> still conditionally rendered based on the previous conditions.
    
    The code has been simplified and made more efficient for better maintainability.

--- pages/signin.js
+++ pages/signin.js
@@ -82,121 +82,119 @@ export default function SignIn({ users }) {
     }
   }
 
-  if (!user) return (
+  return (
     <main className="sm:min-h-[66.66vh] sm:grid sm:grid-cols-5 md:min-h-[82vh] lg:min-h-[calc(100vh_-_6.5rem)] xl:min-h-[95vh] lg:grid-cols-2 xl:grid-cols-5 2xl:grid-cols-3">
       <Head>
-        <title>{`${(email && users.every((user) => user.email !== email)) ? "Register" : "Sign in"} | Game On`}</title>
+        <title>
+          {!user ? (
+            `${(email && users.every((user) => user.email !== email)) ? "Register" : "Sign in"} | Game On`
+          ) : (
+            !user.emailVerified && "Verify Email | Game On"
+          )}
+        </title>
       </Head>
-      <section className="hidden p-10 bg-[linear-gradient(rgba(227,14,48,0.85),rgba(227,14,48,0.85)),url('../public/signin.jpg')] bg-no-repeat bg-cover bg-center sm:flex sm:flex-col sm:justify-center sm:col-span-2 md:p-14 lg:col-span-1 xl:col-span-3 xl:order-2 2xl:col-span-2">
+      <div className="hidden p-10 bg-[linear-gradient(rgba(227,14,48,0.85),rgba(227,14,48,0.85)),url('../public/signin.jpg')] bg-no-repeat bg-cover bg-center sm:flex sm:flex-col sm:justify-center sm:col-span-2 md:p-14 lg:col-span-1 xl:col-span-3 xl:order-2 2xl:col-span-2">
         <div className="space-y-3 xl:w-[60%] xl:mx-auto xl:space-y-4 2xl:w-1/3">
           <p className="relative text-[#cfcfcf] xl:text-lg"><span className="absolute -top-14 -left-4 text-9xl text-[#dd7878] xl:text-[10rem] xl:-top-16 xl:-left-7">"</span>Joining a gaming community is like finding a second family, one that shares your passion for gaming.</p>
           <h2 className="font-semibold text-2xl text-right text-[#f1f1f1] xl:text-3xl">- Game On</h2>
         </div>
-      </section>
-      <section className="px-6 pt-7 pb-14 space-y-20 sm:px-10 md:px-14 sm:col-span-3 sm:flex sm:flex-col sm:justify-between lg:col-span-1 xl:col-span-2 xl:pl-24 2xl:pl-32 2xl:col-span-1">
-        <div className="space-y-8">
-          {isUserNew && (!email || (email && users.some((user) => user.email === email))) && (
-            <div className="space-y-1">
-              <h2 className="text-xl font-semibold">Registration Complete!</h2>
-              <p>You have successfully created your account. Now, you can sign in to your new account using the credentials you provided.</p>
-            </div>
-          )}
-          {!email ? (
-            <div className="space-y-8">
-              <div className="space-y-2">
-                <h1>Sign in</h1>
-                <p>Provide your email address to continue.</p>
+      </div>
+      {!user ? (
+        <section className="px-6 pt-7 pb-14 space-y-20 sm:px-10 md:px-14 sm:col-span-3 sm:flex sm:flex-col sm:justify-between lg:col-span-1 xl:col-span-2 xl:pl-24 2xl:pl-32 2xl:col-span-1">
+          <div className="space-y-8">
+            {isUserNew && (!email || (email && users.some((user) => user.email === email))) && (
+              <div className="space-y-1">
+                <h2 className="text-xl font-semibold">Registration Complete!</h2>
+                <p>You have successfully created your account. Now, you can sign in to your new account using the credentials you provided.</p>
               </div>
+            )}
+            {!email ? (
               <div className="space-y-8">
-                <div className="relative">
-                  <input
-                    className="typing-input pl-12 peer"
-                    ref={emailRef}
-                    type="text"
-                    placeholder="Enter your email"
-                    onKeyUp={(event) => event.key === "Enter" && emailValidation()}
-                    autoFocus
-                  />
-                  <HiMail
-                    className="typing-input--icon"
-                    size={48}
-                    onClick={() => emailRef.current.focus()}
-                  />
+                <div className="space-y-2">
+                  <h1>Sign in</h1>
+                  <p>Provide your email address to continue.</p>
                 </div>
-                <button
-                  className="secondary-btn w-full h-12 hover:bg-[#ffffff]"
-                  type="button"
-                  onClick={() => emailValidation()}
-                >
-                  <p>Next</p>
-                  <IoChevronForward size={13} color="#1f1f1f" />
-                </button>
-              </div>
-            </div>
-          ) : (
-            users.every((user) => user.email !== email) ? (
-              <Register
-                email={email}
-                setEmail={setEmail}
-              />
-            ) : (
-              isProviderOnlyGoogle() ? (
                 <div className="space-y-8">
-                  <div className="space-y-2">
-                    <h2 className="text-2xl font-bold">
-                      Can't Sign in with Password
-                    </h2>
-                    <p>The email address you provided was used to sign in with Google provider last time. In case you provided the wrong email address, you can go back and provide the correct one, otherwise, sign in with Google.</p>
+                  <div className="relative">
+                    <input
+                      className="typing-input pl-12 peer"
+                      ref={emailRef}
+                      type="text"
+                      placeholder="Enter your email"
+                      onKeyUp={(event) => event.key === "Enter" && emailValidation()}
+                      autoFocus
+                    />
+                    <HiMail
+                      className="typing-input--icon"
+                      size={48}
+                      onClick={() => emailRef.current.focus()}
+                    />
                   </div>
                   <button
                     className="secondary-btn w-full h-12 hover:bg-[#ffffff]"
                     type="button"
-                    onClick={() => setEmail("")}
+                    onClick={() => emailValidation()}
                   >
-                    <IoChevronBack size={13} color="#1f1f1f" />
-                    <p className="font-semibold">Back</p>
+                    <p>Next</p>
+                    <IoChevronForward size={13} color="#1f1f1f" />
                   </button>
                 </div>
-              ) : (
-                <EmailAndPasswordSignIn
+              </div>
+            ) : (
+              users.every((user) => user.email !== email) ? (
+                <Register
                   email={email}
                   setEmail={setEmail}
-                  setIsUserLoaded={setIsUserLoaded}
                 />
+              ) : (
+                isProviderOnlyGoogle() ? (
+                  <div className="space-y-8">
+                    <div className="space-y-2">
+                      <h2 className="text-2xl font-bold">
+                        Can't Sign in with Password
+                      </h2>
+                      <p>The email address you provided was used to sign in with Google provider last time. In case you provided the wrong email address, you can go back and provide the correct one, otherwise, sign in with Google.</p>
+                    </div>
+                    <button
+                      className="secondary-btn w-full h-12 hover:bg-[#ffffff]"
+                      type="button"
+                      onClick={() => setEmail("")}
+                    >
+                      <IoChevronBack size={13} color="#1f1f1f" />
+                      <p className="font-semibold">Back</p>
+                    </button>
+                  </div>
+                ) : (
+                  <EmailAndPasswordSignIn
+                    email={email}
+                    setEmail={setEmail}
+                    setIsUserLoaded={setIsUserLoaded}
+                  />
+                )
               )
-            )
-          )}
-        </div>
-        <div className="space-y-8">
-          <div className="grid grid-cols-7 items-center">
-            <hr className="col-span-3 border-[#6f6f6f]" />
-            <p className="col-span-1 text-base text-center text-[#6f6f6f]">
-              or
-            </p>
-            <hr className="col-span-3 border-[#6f6f6f]" />
+            )}
           </div>
-          <GoogleSignIn
-            users={users}
-            setIsUserLoaded={setIsUserLoaded}
-          />
-        </div>
-      </section>
-    </main>
-  );
-  if (user && !user.emailVerified) return (
-    <main className="sm:min-h-[66.66vh] sm:grid sm:grid-cols-5 md:min-h-[82vh] lg:min-h-[calc(100vh_-_6.5rem)] xl:min-h-[95vh] lg:grid-cols-2 xl:grid-cols-5 2xl:grid-cols-3">
-      <Head>
-        <title>Verfiy Email | Game On</title>
-      </Head>
-      <section className="hidden p-10 bg-[linear-gradient(rgba(227,14,48,0.85),rgba(227,14,48,0.85)),url('../public/signin.jpg')] bg-no-repeat bg-cover bg-center sm:flex sm:flex-col sm:justify-center sm:col-span-2 md:p-14 lg:col-span-1 xl:col-span-3 xl:order-2 2xl:col-span-2">
-        <div className="space-y-3 xl:w-[60%] xl:mx-auto xl:space-y-4 2xl:w-1/3">
-          <p className="relative text-[#cfcfcf] xl:text-lg"><span className="absolute -top-14 -left-4 text-9xl text-[#dd7878] xl:text-[10rem] xl:-top-16 xl:-left-7">"</span>Joining a gaming community is like finding a second family, one that shares your passion for gaming.</p>
-          <h2 className="font-semibold text-2xl text-right text-[#f1f1f1] xl:text-3xl">- Game On</h2>
-        </div>
-      </section>
-      <section className="px-6 pt-7 pb-14 space-y-10 sm:px-10 md:px-14 sm:col-span-3 lg:col-span-1 xl:col-span-2 xl:pl-24 2xl:pl-32 2xl:col-span-1">
-        <Verification user={user} />
-      </section>
+          <div className="space-y-8">
+            <div className="grid grid-cols-7 items-center">
+              <hr className="col-span-3 border-[#6f6f6f]" />
+              <p className="col-span-1 text-base text-center text-[#6f6f6f]">
+                or
+              </p>
+              <hr className="col-span-3 border-[#6f6f6f]" />
+            </div>
+            <GoogleSignIn
+              users={users}
+              setIsUserLoaded={setIsUserLoaded}
+            />
+          </div>
+        </section>
+      ) : (
+        !user.emailVerified && (
+          <section className="px-6 pt-7 pb-14 space-y-10 sm:px-10 md:px-14 sm:col-span-3 lg:col-span-1 xl:col-span-2 xl:pl-24 2xl:pl-32 2xl:col-span-1">
+            <Verification user={user} />
+          </section>
+        )
+      )}
     </main>
   );
 }
