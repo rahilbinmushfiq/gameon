a2920cfae59c62decfff8755fec082a9a4d20598
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-04-20 18:12:41 +0600

    fix(gameID): fix game tab padding issue
    This commit fixes the horizontal padding issue of the tab section on game details page for medium-sized screen. The tailwind class `md:p` has been changed to `md:px-14` to resolve the issue.

--- pages/game/[gameID].js
+++ pages/game/[gameID].js
@@ -42,7 +42,7 @@ export default function GameDetails({ gameID, coverImage, name, overview, critic
           </div>
         </div>
       </div>
-      <section className="px-6 py-12 flex gap-2 overflow-auto bg-[#1a1a1a] [&>button]:game-tab sm:px-10 md:p xl:px-24 2xl:px-32">
+      <section className="px-6 py-12 flex gap-2 overflow-auto bg-[#1a1a1a] [&>button]:game-tab sm:px-10 md:px-14 xl:px-24 2xl:px-32">
         <button
           className={tab === "overview" ? "active" : "inactive"}
           onClick={() => setTab("overview")}
