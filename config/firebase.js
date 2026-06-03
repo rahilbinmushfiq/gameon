a4ea51d8976ea4bc7c05e33b93c2f5c35066e878
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2026-06-04 00:03:13 +0200

    chore(firebase): remove unused storage initialization
    This commit removes the obsolete Firebase Storage import and instance export since the project has successfully migrated to Cloudinary.

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,6 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -29,4 +28,3 @@ const app = getFirebaseApp();
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-export const storage = getStorage(app);
